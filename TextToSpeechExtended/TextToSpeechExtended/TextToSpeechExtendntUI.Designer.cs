8caa5e1f5d054dbf95df6ea0c0416e2844d1586c
floatingFaez <floatingfaez@live.com>
2014-10-23 21:02:32 +0600

    Project File Initially Uploaded

---
+++ TextToSpeechExtended/TextToSpeechExtended/TextToSpeechExtendntUI.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace TextToSpeechExtended
+{
+    partial class TextToSpeechExtendntUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadTextButton = new System.Windows.Forms.Button();
+            this.speechTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.resumeButton = new System.Windows.Forms.Button();
+            this.pauseButton = new System.Windows.Forms.Button();
+            this.speakButton = new System.Windows.Forms.Button();
+            this.speechOpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // loadTextButton
+            // 
+            this.loadTextButton.Location = new System.Drawing.Point(378, 9);
+            this.loadTextButton.Name = "loadTextButton";
+            this.loadTextButton.Size = new System.Drawing.Size(197, 24);
+            this.loadTextButton.TabIndex = 0;
+            this.loadTextButton.Text = "Load text from text file";
+            this.loadTextButton.UseVisualStyleBackColor = true;
+            this.loadTextButton.Click += new System.EventHandler(this.loadTextButton_Click);
+            // 
+            // speechTextBox
+            // 
+            this.speechTextBox.Location = new System.Drawing.Point(13, 39);
+            this.speechTextBox.Multiline = true;
+            this.speechTextBox.Name = "speechTextBox";
+            this.speechTextBox.Size = new System.Drawing.Size(562, 238);
+            this.speechTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 290);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Speaker Status : ";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Font = new System.Drawing.Font("Times New Roman", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusLabel.Location = new System.Drawing.Point(139, 290);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(0, 16);
+            this.statusLabel.TabIndex = 2;
+            // 
+            // stopButton
+            // 
+            this.stopButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stopButton.Location = new System.Drawing.Point(501, 283);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(73, 31);
+            this.stopButton.TabIndex = 3;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // resumeButton
+            // 
+            this.resumeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resumeButton.Location = new System.Drawing.Point(422, 283);
+            this.resumeButton.Name = "resumeButton";
+            this.resumeButton.Size = new System.Drawing.Size(73, 31);
+            this.resumeButton.TabIndex = 3;
+            this.resumeButton.Text = "Resume";
+            this.resumeButton.UseVisualStyleBackColor = true;
+            this.resumeButton.Click += new System.EventHandler(this.resumeButton_Click);
+            // 
+            // pauseButton
+            // 
+            this.pauseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pauseButton.Location = new System.Drawing.Point(343, 283);
+            this.pauseButton.Name = "pauseButton";
+            this.pauseButton.Size = new System.Drawing.Size(73, 31);
+            this.pauseButton.TabIndex = 3;
+            this.pauseButton.Text = "Pause";
+            this.pauseButton.UseVisualStyleBackColor = true;
+            this.pauseButton.Click += new System.EventHandler(this.pauseButton_Click);
+            // 
+            // speakButton
+            // 
+            this.speakButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.speakButton.Location = new System.Drawing.Point(264, 283);
+            this.speakButton.Name = "speakButton";
+            this.speakButton.Size = new System.Drawing.Size(73, 31);
+            this.speakButton.TabIndex = 3;
+            this.speakButton.Text = "Speak";
+            this.speakButton.UseVisualStyleBackColor = true;
+            this.speakButton.Click += new System.EventHandler(this.speakButton_Click);
+            // 
+            // speechOpenFileDialog
+            // 
+            this.speechOpenFileDialog.FileName = "speech Dialog";
+            this.speechOpenFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.speechOpenFileDialog_FileOk);
+            // 
+            // TextToSpeechExtendntUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 327);
+            this.Controls.Add(this.speakButton);
+            this.Controls.Add(this.pauseButton);
+            this.Controls.Add(this.resumeButton);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.speechTextBox);
+            this.Controls.Add(this.loadTextButton);
+            this.Name = "TextToSpeechExtendntUI";
+            this.Text = "Text To Speech Extendnt Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadTextButton;
+        private System.Windows.Forms.TextBox speechTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Button resumeButton;
+        private System.Windows.Forms.Button pauseButton;
+        private System.Windows.Forms.Button speakButton;
+        private System.Windows.Forms.OpenFileDialog speechOpenFileDialog;
+    }
+}
+
---
+++ TextToSpeechExtended/TextToSpeechExtended/TextToSpeechExtendntUI.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.IO;
+
+namespace TextToSpeechExtended
+{
+    public partial class TextToSpeechExtendntUI : Form
+    {
+        private SpeechSynthesizer aSynthesizer;
+        public TextToSpeechExtendntUI()
+        {
+            InitializeComponent();
+            aSynthesizer = new SpeechSynthesizer();
+            pauseButton.Enabled = false;
+            resumeButton.Enabled = false;
+            stopButton.Enabled = false;
+            speechTextBox.ScrollBars = ScrollBars.Both;
+        }
+
+        private void speakButton_Click(object sender, EventArgs e)
+        {
+            aSynthesizer.Dispose();
+            if (speechTextBox.Text!="")
+            {
+                aSynthesizer = new SpeechSynthesizer();
+                aSynthesizer.SpeakAsync(speechTextBox.Text);
+                aSynthesizer.Volume = 100;
+                aSynthesizer.Rate = -5;
+                statusLabel.Text = @"Speaking";
+                pauseButton.Enabled = true;
+                stopButton.Enabled = true;
+                aSynthesizer.SpeakCompleted += new EventHandler<SpeakCompletedEventArgs>(aSynthesizer_SpeakCompleted);
+            }
+            else
+            {
+                MessageBox.Show(@"Please enter some text in the textbox", @"Message", MessageBoxButtons.OK);
+            }
+        }
+        void aSynthesizer_SpeakCompleted(object sender, SpeakCompletedEventArgs e)
+        {
+            statusLabel.Text = @"Idle";
+        }
+
+        private void pauseButton_Click(object sender, EventArgs e)
+        {
+            if (aSynthesizer != null)
+            {
+                if (aSynthesizer.State == SynthesizerState.Speaking)
+                {
+                    aSynthesizer.Pause();
+                    statusLabel.Text = @"Paused";
+                    resumeButton.Enabled = true;
+                }
+            }
+        }
+
+        private void resumeButton_Click(object sender, EventArgs e)
+        {
+            if (aSynthesizer != null)
+            {
+                if (aSynthesizer.State == SynthesizerState.Paused)
+                {
+                    aSynthesizer.Resume();
+                    statusLabel.Text = @"Speaking";
+                }
+                resumeButton.Enabled = false;
+            } 
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+
+            if (aSynthesizer != null)
+            {
+                aSynthesizer.Dispose();
+                statusLabel.Text = @"Idle";
+                pauseButton.Enabled = false;
+                resumeButton.Enabled = false;
+                stopButton.Enabled = false;
+            } 
+        }
+
+        private void loadTextButton_Click(object sender, EventArgs e)
+        {
+            speechOpenFileDialog.ShowDialog();
+        }
+
+        private void speechOpenFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            speechTextBox.Text = File.ReadAllText(speechOpenFileDialog.FileName.ToString());
+        } 
+       
+    }
+}
